9883e3df620b960f25a94225c696b759c455e976
tomas <tomas22tomas@gmail.com>
2020-12-10 17:51:48 +0100

    OptionsMenu

--- Assets/Scripts/Interface/ChangerScene.cs
+++ Assets/Scripts/Interface/ChangerScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuManager : MonoBehaviour
+public class ChangerScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
