fce56339dac060de9b4d5aa1255a71c27b8778c9
pauula14 <paulacalzada014@gmail.com>
2020-12-27 10:55:15 +0100

    modo carrera arreglado jeje

--- Assets/Scripts/Input/InputRunnerMode.cs
+++ Assets/Scripts/Input/InputRunnerMode.cs
@@ -9,7 +9,7 @@ public class InputRunnerMode : MonoBehaviour
 
     //CONTROL DEL PLAYER
     [SerializeField] float jumpForce = 10;
-    [SerializeField] float speed = 3;
+    [SerializeField] float speed = 5;
     [SerializeField] float maxSpeed = 10;
 
     private PlayerControls _controls;
@@ -40,7 +40,8 @@ public class InputRunnerMode : MonoBehaviour
     private bool tirar = false;
     private float _timeFall;
 
-    
+    private bool isHitted = false;
+
     //empujon
     private double _timeAttacking;
     private bool isAttacking = false;
@@ -77,17 +78,8 @@ public class InputRunnerMode : MonoBehaviour
 
     private void FixedUpdate()
     {
-        playerInput = new Vector3(1, 0, _horizontaldirection.x);
+        playerInput = new Vector3(_horizontaldirection.x, 0, 1);
         playerInput = Vector3.ClampMagnitude(playerInput, 1); //para poder normalizar la distancia. 1 es el valor max, va de 0 a 1      
-
-
-        //Para que no supere una velocidad:
-        /*if(_playerRB.velocity.magnitude > maxSpeed)
-        {
-            _playerRB.velocity = _playerRB.velocity.normalized * maxSpeed;
-        }*/
-
-        //playerDirection = playerInput.x * camRight + playerInput.z * camFordward; //Almacena la direccion hacia la que se esta moviendo el player
 
         _playerRB.transform.LookAt(_playerRB.transform.position + playerInput);
 
@@ -97,24 +89,19 @@ public class InputRunnerMode : MonoBehaviour
 
         target.transform.LookAt(pivot);
 
-        //ToRun(Time.fixedTime); //Comprueba si sigue deslizandose o no
         FishRun(Time.fixedTime); //Velocidad despues de comer el pez
-        ToStand(Time.fixedTime);
-        //ToThrowStocks(Time.fixedTime);
-
 
         //movimiento
         if (toStop == false)
         {
-            _playerRB.AddForce(Vector3.left * 10, ForceMode.Impulse);
+            _playerRB.AddForce(Vector3.forward * 10, ForceMode.Acceleration);
             _playerRB.velocity = new Vector3(playerInput.x * speed, _playerRB.velocity.y, playerInput.z * speed);
         }
         else
-        {
-            
+        {           
             if (Time.fixedTime < _timeTillStop)
             {
-                _playerRB.AddForce(Vector3.left * 10, ForceMode.Impulse);
+                _playerRB.AddForce(Vector3.left * 10, ForceMode.Acceleration);
                 _playerRB.velocity = new Vector3(playerInput.x * speed, _playerRB.velocity.y, playerInput.z * speed);
             }
         }
@@ -128,7 +115,7 @@ public class InputRunnerMode : MonoBehaviour
         {
             inFloor = false;
             penguinPos = _playerRB.position;
-            _playerRB.MovePosition(new Vector3(penguinPos.x, penguinPos.y + 10, penguinPos.z - 10)); //la coloca en medio, si la cuesta es recta no hace falta dcha izq, sino si
+            _playerRB.MovePosition(new Vector3(penguinPos.x - 10, penguinPos.y + 10, penguinPos.z)); //la coloca en medio, si la cuesta es recta no hace falta dcha izq, sino si
             Debug.Log("ME CAIGO");
         }
 
@@ -136,24 +123,24 @@ public class InputRunnerMode : MonoBehaviour
         {
             inFloor = false;
             penguinPos = _playerRB.position;
-            _playerRB.MovePosition(new Vector3(penguinPos.x, penguinPos.y + 10, penguinPos.z + 10)); //la coloca en medio, si la cuesta es recta no hace falta dcha izq, sino si
+            _playerRB.MovePosition(new Vector3(penguinPos.x + 10, penguinPos.y + 10, penguinPos.z)); //la coloca en medio, si la cuesta es recta no hace falta dcha izq, sino si
             Debug.Log("ME CAIGO");
         }
 
-        if (collision.gameObject.tag == "Goal") //Si esta en el mapa
+        if (collision.gameObject.tag == "Goal") //LLEGA A LA META
         {
             Debug.Log("Oleee");
         }
 
-        if (collision.gameObject.tag == "LeftPenguin") //Si choca con un pescao
+        if (collision.gameObject.tag == "LeftPenguin") //Si choca con el lado izq de un pingu
         {
-
+            isHitted = true;
             WasHitted(false);
         }
 
-        if (collision.gameObject.tag == "RightPenguin") //Si choca con un pescao
+        if (collision.gameObject.tag == "RightPenguin") //Si choca con el lado dch de un pingu
         {
-
+            isHitted = true;
             WasHitted(true);
         }
     }
@@ -175,44 +162,27 @@ public class InputRunnerMode : MonoBehaviour
             int numRand = Random.Range(1, 4);
             Debug.Log(numRand);
             _timeTillStop = Time.fixedTime + numRand;
-            toStop = true;
-            
+            toStop = true;            
         }
 
         if (collision.gameObject.tag == "Floor") //Si esta en el mapa
         {
 
             Debug.Log("SUELO");
-            speed = 5;
-            
+            //_playerRB.AddForce(Vector3.left * 10, ForceMode.Acceleration);
+            //_playerRB.velocity = new Vector3(playerInput.x * speed, _playerRB.velocity.y, playerInput.z * speed);
             inFloor = true;
-            //InIceDashPlat = false;
         }
 
 
-        if (collision.gameObject.tag == "Penguin") //Si choca con un pinguino
+       /* if (collision.gameObject.tag == "Penguin") //Si choca con un pinguino
         {
             if (isAttacking == true) //si estas dando colleja
             {
-                //_timeFall = Time.fixedTime + 3;
-                //Rigidbody enemy = collision.gameObject.GetComponent<Rigidbody>(); //le tiras
-                //enemy.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
-
                 //LE MANDAS AL OTRO A CAERSE -> EJECUTAR TO FALL
                 isAttacking = false;
             }
-        }
-
-
-
-        //PRUEBAS
-        if (collision.gameObject.tag == "caca") //Si choca con un pescao
-        {
-
-            ToDie();
-            //
-            mainCamera = collision.gameObject.GetComponent<Camera>();
-        }
+        }*/
     }
     #endregion
 
@@ -224,8 +194,7 @@ public class InputRunnerMode : MonoBehaviour
         //Añade el evento moverse
         _controls.Player.Movement.performed += Move;
         _controls.Player.Attack.performed += Attack; //Evento atacar
-        _controls.Player.Run.performed += Run; //Evento correr
-        //_controls.Player.PowerUp.performed += PowerUp; //Evento power up
+        _controls.Player.Run.performed += Jump; //Evento correr
         _controls.Player.CameraControl.performed += GetCameraMove;//Movimiento de camara
 
         //Habilita el evento
@@ -238,8 +207,7 @@ public class InputRunnerMode : MonoBehaviour
         //Elimina el evento
         _controls.Player.Movement.canceled -= Move;
         _controls.Player.Attack.canceled -= Attack;
-        _controls.Player.Run.canceled -= Run;
-        //_controls.Player.PowerUp.canceled -= PowerUp;
+        _controls.Player.Run.canceled -= Jump;
         _controls.Player.CameraControl.canceled -= GetCameraMove;
         _controls.Player.Disable();
 
@@ -258,21 +226,18 @@ public class InputRunnerMode : MonoBehaviour
         Debug.Log("BOFETÓN");
         //tiene que impulsar a otro pingu
         //CAMBIAR ANIMACIÓN
-        //AÑADIR LO Q SEA PARA LA BOFETADA
-        //tirar = true;
         isAttacking = true;
         _timeAttacking = Time.fixedTime; //tiempo que estará activo el ataque
     }
 
-    public void Run(InputAction.CallbackContext context) //De momento va a ser saltar
+    public void Jump(InputAction.CallbackContext context) //De momento va a ser saltar
     {
-        Debug.Log("DESLIZA");
-        //forceDirection = _playerRB.transform.forward;
-        //isRunning = true;
-        speed = 10;
-        //_timeRunning = Time.fixedTime + 10;
-        Debug.Log("vel " + speed);
-        //CAMBIAR ANIMACIÓN
+        if (inFloor == true)
+        {
+            Debug.Log("SALTO");
+            _playerRB.AddForce(Vector3.up * 3.5f, ForceMode.Impulse);
+            inFloor = false;
+        }   
     }
 
 
@@ -287,31 +252,6 @@ public class InputRunnerMode : MonoBehaviour
 
     #region ACTIONS TIME
 
-    //Controla el tiempo que esta activo el deslizamiento
-    /*public void ToRun(double deltaTime)
-    {
-        if (isRunning == true)
-        {
-            Debug.Log("a correr");
-            Debug.Log("delta time " + deltaTime);
-            Debug.Log("tiempo pasado " + _timeRunning);
-            //_controls.Player.Movement.Disable();
-            //playerInput = new Vector3(playerInput.x, 0, playerInput.z);
-            //_playerRB.velocity = new Vector3(playerInput.x * 0.5f * speed, _playerRB.velocity.y, playerDirection.z * 0.5f * speed);
-            _playerRB.AddForce(forceDirection * speed, ForceMode.Acceleration);
-            //_playerRB.AddForce(playerInput * 0.25f, ForceMode.Impulse);
-
-            if (deltaTime > _timeRunning)
-            {
-                speed = 3;
-                Debug.Log("delta time " + Time.deltaTime);
-                Debug.Log("tiempo pasado " + _timeRunning);
-                _playerRB.AddForce(forceDirection * 0, ForceMode.Acceleration);
-                //_controls.Player.Movement.Enable();
-                isRunning = false;
-            }
-        }
-    }*/
 
     //Tiempo que dura con la velocidad por el pez
     public void FishRun(double deltaTime)
@@ -333,62 +273,30 @@ public class InputRunnerMode : MonoBehaviour
         }
     }
 
-    //Se levanta despues de x tiempo caído
-    public void ToStand(double deltaTime)
-    {
-        if (_playerRB.transform.rotation == Quaternion.Euler(0f, 0f, 90f))
-        {
-            if (deltaTime > _timeFall)
-            {
-                _playerRB.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            }
-        }
-    }
-
     public void WasHitted(bool side) //false = izq, true = dch
-    {
-        Debug.Log("fuiste pegado");
+    { 
 
-        if (side == false)
+        if (isHitted == true)
         {
-            Debug.Log("LEFT");
-            _playerRB.AddForce(Vector3.forward * -100, ForceMode.Impulse);
-            //penguinPos = _playerRB.position;
-            //_playerRB.MovePosition(new Vector3(penguinPos.x, penguinPos.y, penguinPos.z - 5));
-        }
-        else
-        {
-            Debug.Log("TRUE");
-            _playerRB.AddForce(Vector3.forward * 100, ForceMode.Impulse);
-        }
-        
-    }
+            Debug.Log("fuiste pegado");
 
-    //Caerse
-    public void ToFall()
-    {
-        _timeFall = Time.fixedTime + 3;
-        _playerRB.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
-    }
-
-    //morir
-    public void ToDie()
-    {
-        _controls.Player.Disable();
-        Destroy(gameObject);
-    }
-    /*
-    //Comprueba que puede tirar el cepo
-    public void ToThrowStocks(double deltaTime)
-    {
-        if (cepoActive == true)
-        {
-            if (Time.fixedTime > timeAwait)
+            if (side == false)
             {
-                cepoActive = false;
+                Debug.Log("LEFT");
+                _playerRB.AddForce(Vector3.left * 25, ForceMode.Impulse);
+                //penguinPos = _playerRB.position;
+                //_playerRB.MovePosition(new Vector3(penguinPos.x, penguinPos.y, penguinPos.z - 5));
+                isHitted = false;
+            }     
+            else
+            {
+                Debug.Log("TRUE");
+                _playerRB.AddForce(Vector3.right * 25, ForceMode.Impulse);
+                isHitted = false;
             }
-        }
-    }*/
+        }           
+    }
+
 
     #endregion
 
@@ -425,8 +333,8 @@ public class InputRunnerMode : MonoBehaviour
         moveY = Mathf.Clamp(moveY, -50.0f, 70.0f);
 
         //pivot sigue a player
-        Vector3 follow = new Vector3(this.transform.position.x, this.transform.position.y /*- 1.0f*/, this.transform.position.z);
-        pivot.position = Vector3.Lerp(pivot.position, follow /*+ offset*/, Time.deltaTime * 100);
+        Vector3 follow = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        pivot.position = Vector3.Lerp(pivot.position, follow, Time.deltaTime * 100);
 
         //Rotar pivot
         pivot.rotation = Quaternion.Euler(-moveY, moveX, 0.0f);
