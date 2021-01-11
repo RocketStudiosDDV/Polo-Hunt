2367622cd913c3c2d67e52d7019ae6e962622fcb
pauula14 <paulacalzada014@gmail.com>
2021-01-12 00:01:51 +0100

    controles 99% funcionales mobile

--- Assets/Scripts/Interface/DeviceUsing.cs
+++ Assets/Scripts/Interface/DeviceUsing.cs
@@ -12,12 +12,36 @@ public class DeviceUsing : MonoBehaviour
         canvasHUD = GetComponent<Canvas>();
         mobileHUD = canvasHUD.transform.Find("InputMobile").gameObject as GameObject;
 
-        if (SystemInfo.deviceType == DeviceType.Handheld) //si es movil
+        /*if (SystemInfo.deviceType == DeviceType.Handheld) //si es movil
         {
             Debug.Log("SOY UN MOVIL");
             mobileHUD.SetActive(true);
         }
         else //es ordenador
+        {
+            Debug.Log("SOY UN ORDENAODR");
+            mobileHUD.SetActive(false);
+        }*/
+
+
+        /*if (Application.platform == RuntimePlatform.Android)
+        {
+            Debug.Log("SOY UN MOVIL");
+            mobileHUD.SetActive(true);
+        }
+
+        if ((Application.platform == RuntimePlatform.WindowsPlayer)||(Application.isMobilePlatform)
+        {
+            Debug.Log("SOY UN ORDENAODR");
+            mobileHUD.SetActive(false);
+        }*/
+
+        if ((Application.isMobilePlatform))
+        {
+            Debug.Log("SOY UN MOVIL");
+            mobileHUD.SetActive(true);
+        }
+        else
         {
             Debug.Log("SOY UN ORDENAODR");
             mobileHUD.SetActive(false);
